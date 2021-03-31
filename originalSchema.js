ba8bd1f810bba475ccca6f300a817169ee8db210
jakeblaxon <42847286+jakeblaxon@users.noreply.github.com>
2021-03-30 23:01:09 -0500

    populated book data

--- originalSchema.js
+++ originalSchema.js
@@ -31,15 +31,18 @@ exports.originalSchema = makeExecutableSchema({
     }
   }
 });
+
 const authors = [
-  { id: "1", name: `A` },
-  { id: "2", name: `B` },
-  { id: "3", name: `C` }
+  { id: "1", name: `William Shakespeare` },
+  { id: "2", name: `Jane Austen` },
+  { id: "3", name: `Charles Dickens` }
 ];
+
 const books = [
-  { id: "1", title: "title A", authorId: "3" },
-  { id: "1", title: "title A", authorId: "1" },
-  { id: "1", title: "title A", authorId: "3" },
-  { id: "1", title: "title A", authorId: "2" },
-  { id: "1", title: "title A", authorId: "3" }
+  { id: "1", title: "Hamlet", authorId: "1" },
+  { id: "2", title: "Great Expectations", authorId: "3" },
+  { id: "3", title: "Pride and Prejudice", authorId: "2" },
+  { id: "4", title: "Romeo & Juliet", authorId: "1" },
+  { id: "5", title: "Macbeth", authorId: "1" },
+  { id: "6", title: "A Tale of Two Cities", authorId: "3" }
 ];
